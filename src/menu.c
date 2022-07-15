8e3c67c5f927af1cc27e301f9ad31dc745d15e3b
Aeonn <theodebauvais@protonmail.com>
2022-07-15 16:28:36 +0200

    Modified a bit menu files

--- src/menu.c
+++ src/menu.c
@@ -1,20 +1,22 @@
 #include "menu.h"
+#include "list_pokemon.h"
 
 
-int menu(head_list_pokemon **linked_list){
+void menu(head_list_pokemon **linked_list){
 	printf("========== Welcome to your Pokedex ! ========== \n");
 	printf("What do you want to do today ? \n");
-	printf("1 - Add a pokemon seen ? \n2 - Add a pokemon captured ? \n3 - Display all pokemon ?\n4 - Display one pokemon ?\n5 - Display pokemon captured ?\n6 - Modify a pokemon ?\n7 - Delete a pokemon ?\n8 - Save the Pokedex ?\n");
-	printf("Your choice : \n");
-	int index = 0;
-	scanf("%d", &index);
-	if (index < 1 || index > 8){
-		printf("Bad number !");
-		return -1;
+	while(1) {
+		printf("1 - Add a pokemon seen ? \n2 - Add a pokemon captured ? \n3 - Display all pokemon ?\n4 - Display one pokemon ?\n5 - Display pokemon captured ?\n6 - Modify a pokemon ?\n7 - Delete a pokemon ?\n8 - Save the Pokedex ?\n");
+		printf("Your choice : \n");
+		int index = 0;
+		scanf("%d", &index);
+		if (index < 1 || index > 8){
+			printf("Bad number !\n");
+			continue;
+		}
+		index -= 1;
+		select_action(index, linked_list);
 	}
-	index -= 1;
-	select_action(index, linked_list);
-	return 0;
 }
 
 char* get_now_as_str() {
@@ -29,33 +31,33 @@ char* get_now_as_str() {
 }
 
 int add_pokemon_seen(head_list_pokemon **linked_list){
-	char *name = NULL;
-	char *type_one = NULL;
-	char *type_two = NULL;	
-	char *first_capacity = NULL;
+	char *name = calloc(20, sizeof (char));
+	char *type_one = calloc(20, sizeof (char));
+	char *type_two = calloc(20, sizeof (char));	
+	char *first_capacity = calloc(20, sizeof (char));
 	printf("Well done ! \nWhich pokemon do you want to add ?\n");
-	printf("Name : \n");
+	printf("Name : ");
 	scanf("%s", name);
 	while(name == NULL){
 		printf("Name can not be null\n");
-			printf("Name : \n");
-			scanf("%s", name);
+			printf("Name : ");
+			scanf(" %s", name);
 	}
-	printf("First type : \n");
-	scanf("%s", type_one);
+	printf("First type : ");
+	scanf(" %s", type_one);
 	while(type_one == NULL){
 		printf("First type can not be null\n");
-		printf("First type : \n");
-		scanf("%s", type_one);
+		printf("First type : ");
+		scanf(" %s", type_one);
 	}	
-	printf("Second type (can be empty): \n");
-	scanf("%s", type_two);
-	printf("Capacity : \n");
-	scanf("%s", first_capacity);	
+	printf("Second type (can be empty): ");
+	scanf(" %s", type_two);
+	printf("Capacity : ");
+	scanf(" %s", first_capacity);	
 	while(first_capacity == NULL){
 		printf("Capacity can not be null\n");
-		printf("Capacity : \n");
-		scanf("%s", first_capacity);
+		printf("Capacity : ");
+		scanf(" %s", first_capacity);
 	}
 	
 	struct pokemon_crud new_poke;
@@ -69,7 +71,6 @@ int add_pokemon_seen(head_list_pokemon **linked_list){
 		
 	list_functions[ADD](linked_list, new_poke);
 	return 0;
-	
 }
 
 void select_action(int index, head_list_pokemon **linked_list){
